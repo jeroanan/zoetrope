2965102e62a8df879b883c4ca53fbd731ee20ec5
jeroanan <david.tosslehoff@gmail.com>
2015-11-24 22:06:06 +0000

    task class

--- Task.py
+++ Task.py
@@ -1,18 +1,58 @@
 class Task(object):
 
     def __init__(self, task_string):
-        self.__name = ""
+        self.__name = ''
+        self.__workunit_name = ''
+        self.__project_url = ''
+        self.__report_deadline = ''
+        self.__ready_to_report = ''
+        self.__got_server_ack = ''
+        self.__final_cpu_time = ''
+        self.__state = ''
+        self.__scheduler_state = ''
+        self.__exit_status = ''
+        self.__signal = ''
+        self.__suspended_via_gui = ''
+        self.__active_task_state = ''
+        self.__app_version_num = ''
+        self.__checkpoint_cpu_time = ''
+        self.__current_cpu_time = ''
+        self.__fraction_done = ''
+        self.__swap_size = ''
+        self.__working_set_size = ''
+        self.__estimated_cpu_time_remaining = ''
 
         task_strings = task_string.split('\n')
 
+        line_mappings = {'name': 'name',
+                         'WU name': 'workunit_name',
+                         'project URL': 'project_url',
+                         'report deadline': 'report_deadline',
+                         'ready to report': 'ready_to_report',
+                         'got server ack': 'got_server_ack',
+                         'final CPU time': 'final_cpu_time',
+                         'state': 'state',
+                         'scheduler state': 'scheduler_state',
+                         'exit_status': 'exit_status',
+                         'signal': 'signal',
+                         'suspended via GUI': 'suspended_via_gui',
+                         'active_task_state': 'active_task_state',
+                         'app version num': 'app_version_num',
+                         'checkpoint CPU time': 'checkpoint_cpu_time',
+                         'current CPU time': 'current_cpu_time',
+                         'fraction done': 'fraction_done',
+                         'swap size': 'swap_size',
+                         'working set size': 'working_set_size',
+                         'estimated CPU time remaining': 'estimated_cpu_time_remaining'}
+
         for ts in task_strings:
             line_split = ts.split(':')
-            self.__set_name(line_split)
+            line_split.reverse()
+            key = line_split.pop().strip()
+            if key in line_mappings:
+                line_split.reverse()
+                setattr(self, line_mappings[key], str.join(':', line_split).strip())
 
-    def __set_name(self, line_split):
-        if line_split[0].strip().startswith('name'):
-            self.name = str.join(' ', line_split[1:])
-            
     @property
     def name(self):
         return self.__name
@@ -20,3 +60,155 @@ class Task(object):
     @name.setter
     def name(self, val):
         self.__name = val
+
+    @property
+    def workunit_name(self):
+        return self.__workunit_name
+
+    @workunit_name.setter
+    def workunit_name(self, val):
+        self.__workunit_name = val
+
+    @property
+    def project_url(self):
+        return self.__project_url
+
+    @project_url.setter
+    def project_url(self, val):
+        self.__project_url = val
+
+    @property
+    def report_deadline(self):
+        return self.__report_deadline
+
+    @report_deadline.setter
+    def report_deadline(self, val):
+        self.__report_deadline = val
+
+    @property
+    def ready_to_report(self):
+        return self.__ready_to_report
+
+    @ready_to_report.setter
+    def ready_to_report(self, val):
+        self.__ready_to_report = val
+
+    @property
+    def got_server_ack(self):
+        return self.__got_server_ack
+
+    @got_server_ack.setter
+    def got_server_ack(self, val):
+        self.__got_server_ack = val
+
+    @property
+    def final_cpu_time(self):
+        return self.__final_cpu_time
+
+    @final_cpu_time.setter
+    def final_cpu_time(self, val):
+        self.__final_cpu_time = val
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, val):
+        self.__state = val
+
+    @property
+    def scheduler_state(self):
+        return self.__scheduler_state
+
+    @scheduler_state.setter
+    def scheduler_state(self, val):
+        self.__scheduler_state = val
+
+    @property
+    def exit_status(self):
+        return self.__exit_status
+
+    @exit_status.setter
+    def exit_status(self, val):
+        self.__exit_status = val
+
+    @property
+    def signal(self):
+        return self.__signal
+
+    @signal.setter
+    def signal(self, val):
+        self.__signal = val
+
+    @property
+    def suspended_via_gui(self):
+        return self.__suspended_via_gui
+
+    @suspended_via_gui.setter
+    def suspended_via_gui(self, val):
+        self.__suspended_via_gui = val
+
+    @property
+    def active_task_state(self):
+        return self.__active_task_state
+
+    @active_task_state.setter
+    def active_task_state(self, val):
+        self.__active_task_state = val
+
+    @property
+    def app_version_num(self):
+        return self.__app_version_num
+
+    @app_version_num.setter
+    def app_version_num(self, val):
+        self.__app_version_num = val
+
+    @property
+    def checkpoint_cpu_time(self):
+        return self.__checkpoint_cpu_time
+
+    @checkpoint_cpu_time.setter
+    def checkpoint_cpu_time(self, val):
+        self.__checkpoint_cpu_time = val
+
+    @property
+    def current_cpu_time(self):
+        return self.__current_cpu_time
+
+    @current_cpu_time.setter
+    def current_cpu_time(self, val):
+        self.__current_cpu_time = val
+
+    @property
+    def fraction_done(self):
+        return self.__fraction_done
+
+    @fraction_done.setter
+    def fraction_done(self, val):
+        self.__fraction_done = val
+
+    @property
+    def swap_size(self):
+        return self.__swap_size
+
+    @swap_size.setter
+    def swap_size(self, val):
+        self.__swap_size = val
+
+    @property
+    def working_set_size(self):
+        return self.__working_set_size
+
+    @working_set_size.setter
+    def working_set_size(self, val):
+        self.__working_set_size = val
+
+    @property
+    def estimated_cpu_time_remaining(self):
+        return self.__estimated_cpu_time_remaining
+
+    @estimated_cpu_time_remaining.setter
+    def estimated_cpu_time_remaining(self, val):
+        self.__estimated_cpu_time_remaining = val
---
+++ tests/TestTask.py
@@ -0,0 +1,109 @@
+import Task as t
+import unittest
+
+class tests(unittest.TestCase):
+
+    def setUp(self):
+        self.__test_data = \
+"""   name: p2030.20141112.G191.15-00.25.S.b2s0g0.00000_528_0
+   WU name: p2030.20141112.G191.15-00.25.S.b2s0g0.00000_528
+   project URL: http://einstein.phys.uwm.edu/
+   report deadline: Tue Dec  8 15:33:47 2015
+   ready to report: no
+   got server ack: no
+   final CPU time: 1000.000000
+   state: 2
+   scheduler state: 0
+   exit_status: 0
+   signal: 0
+   suspended via GUI: no
+   active_task_state: 0
+   app version num: 0
+   checkpoint CPU time: 1330.000000
+   current CPU time: 1414.000000
+   fraction done: 88.000000
+   swap size: 10.000000
+   working set size: 11.000000
+   estimated CPU time remaining: 74181.872265"""
+
+        self.__target = t.Task(self.__test_data)
+
+    def test_name(self):
+        expected_value = 'p2030.20141112.G191.15-00.25.S.b2s0g0.00000_528_0'
+        self.assertEqual(expected_value, self.__target.name)
+
+    def test_workunit_name(self):
+        expected_value = 'p2030.20141112.G191.15-00.25.S.b2s0g0.00000_528'
+        self.assertEqual(expected_value, self.__target.workunit_name)
+
+    def test_project_url(self):
+        expected_value = 'http://einstein.phys.uwm.edu/'
+        self.assertEqual(expected_value, self.__target.project_url)
+
+    def test_report_deadline(self):
+        expected_value = 'Tue Dec  8 15:33:47 2015'
+        self.assertEqual(expected_value, self.__target.report_deadline)
+
+    def test_ready_to_report(self):
+        expected_value = 'no'
+        self.assertEqual(expected_value, self.__target.ready_to_report)
+
+    def test_got_server_ack(self):
+        expected_value = 'no'
+        self.assertEqual(expected_value, self.__target.got_server_ack)
+
+    def test_final_cpu_time(self):
+        expected_value = '1000.000000'
+        self.assertEqual(expected_value, self.__target.final_cpu_time)
+
+    def test_state(self):
+        expected_value = '2'
+        self.assertEqual(expected_value, self.__target.state)
+
+    def test_scheduler_state(self):
+        expected_value = '0'
+        self.assertEqual(expected_value, self.__target.scheduler_state)
+
+    def test_exit_status(self):
+        expected_value = '0'
+        self.assertEqual(expected_value, self.__target.exit_status)
+
+    def test_signal(self):
+        expected_value = '0'
+        self.assertEqual(expected_value, self.__target.signal)
+
+    def test_suspended_via_gui(self):
+        expected_value = 'no'
+        self.assertEqual(expected_value, self.__target.suspended_via_gui)
+
+    def test_active_task_state(self):
+        expected_value = '0'
+        self.assertEqual(expected_value, self.__target.active_task_state)
+
+    def test_app_version_num(self):
+        expected_value = '0'
+        self.assertEqual(expected_value, self.__target.app_version_num)
+
+    def test_checkpoint_cpu_time(self):
+        expected_value = '1330.000000'
+        self.assertEqual(expected_value, self.__target.checkpoint_cpu_time)
+
+    def test_current_cpu_time(self):
+        expected_value = '1414.000000'
+        self.assertEqual(expected_value, self.__target.current_cpu_time)
+
+    def test_fraction_done(self):
+        expected_value = '88.000000'
+        self.assertEqual(expected_value, self.__target.fraction_done)
+
+    def test_swap_size(self):
+        expected_value = '10.000000'
+        self.assertEqual(expected_value, self.__target.swap_size)
+
+    def test_working_set_size(self):
+        expected_value = '11.000000'
+        self.assertEqual(expected_value, self.__target.working_set_size)
+
+    def test_estimated_cpu_time_remaining(self):
+        expected_value = '74181.872265'
+        self.assertEqual(expected_value, self.__target.estimated_cpu_time_remaining)
