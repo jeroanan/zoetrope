f9f5728f1bdfca9ac860c36cf6f925f1543f2bb9
jeroanan <david.tosslehoff@gmail.com>
2016-05-05 23:47:35 +0100

    Add friendly message if task not found

--- boincsite/templates/assets/js/controllers/taskCtrl.js
+++ boincsite/templates/assets/js/controllers/taskCtrl.js
@@ -11,25 +11,37 @@ TaskController.$inject = ['$http', '$routeParams', 'taskSvc', 'projectsSvc'];
 function TaskController($http, $routeParams, taskSvc, projectsSvc) {
 
   var vm = this;
-
-  taskSvc.get()().query().$promise.then(function(d) {
-    var task = d;
-
-    projectsSvc.get()().query().$promise.then(function(d) {
-      var projects = d;
-
-      task.project_name = get_project_name(task, projects);
-      task.state = get_state_string(task);
-      task.time_so_far = get_time_so_far(task);
-      vm.task = task;
-
-      vm.suspend_button_text = task.suspended_via_gui ? 'Resume' : 'Suspend';
-      vm.ready = true;
-    })
-  })
-
-
   vm.ready = false;
   vm.title = 'Task Summary';
+  vm.task = {};
+  vm.suspend_button_text = '';
+  vm.error = '';
+
+  function onError() {
+	 var errorText = 'Task not found';
+	 vm.ready = true;
+	 vm.error = true;
+	 vm.errorMessage = errorText;	 
+  }
+
+  function gotTask(task) {
+	 projectsSvc.get()().query().$promise.then(
+		function(projects) {
+		  gotProjects(projects, task);
+		}, onError);
+  }
+
+  function gotProjects(projects, task) {
+	 task.project_name = get_project_name(task, projects);
+	 task.state = get_state_string(task);
+	 task.time_so_far = get_time_so_far(task);
+	 vm.task = task;
+	 
+	 vm.suspend_button_text = task.suspended_via_gui ? 'Resume' : 'Suspend';
+	 vm.ready = true;
+  }
+  
+  taskSvc.get()().query().$promise.then(gotTask, onError);
+  
   document.title = vm.title;
 }
