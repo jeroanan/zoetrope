42683d793a4793efcb8a2bb2fc0381a4c5ee6cb0
jeroanan <david.tosslehoff@gmail.com>
2016-05-17 20:39:29 +0100

    move minified controllers and services up a level so I can just do *.js in the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,42 +3,11 @@ module.exports = function(grunt) {
   var app = 'boincsite/templates/assets/js/app.js';
 
   var controllers= [
-    'boincsite/templates/assets/js/controllers/controller.js',
-    'boincsite/templates/assets/js/controllers/diskUsageCtrl.js',
-    'boincsite/templates/assets/js/controllers/indexCtrl.js',
-    'boincsite/templates/assets/js/controllers/taskCtrl.js',
-    'boincsite/templates/assets/js/controllers/projectsCtrl.js',
-    'boincsite/templates/assets/js/controllers/projectCtrl.js',
-    'boincsite/templates/assets/js/controllers/hostInfoCtrl.js',
-    'boincsite/templates/assets/js/controllers/dailyTransferCtrl.js',
-    'boincsite/templates/assets/js/controllers/messagesCtrl.js',
-    'boincsite/templates/assets/js/controllers/noticesCtrl.js',
-    'boincsite/templates/assets/js/controllers/globalPreferencesCtrl.js',
-    'boincsite/templates/assets/js/controllers/allProjectListCtrl.js',
-    'boincsite/templates/assets/js/controllers/projectDetailCtrl.js',
-    'boincsite/templates/assets/js/controllers/attachProjectCtrl.js',
-    'boincsite/templates/assets/js/controllers/detachProjectCtrl.js'
+    'boincsite/templates/assets/js/controllers/*.js',
   ];
 
   var services = [
-    'boincsite/templates/assets/js/services/services.js',
-    'boincsite/templates/assets/js/services/dailyTransferHistorySvc.js',
-    'boincsite/templates/assets/js/services/tasksSvc.js',
-    'boincsite/templates/assets/js/services/projectsSvc.js',
-    'boincsite/templates/assets/js/services/projectSvc.js',
-    'boincsite/templates/assets/js/services/messagesSvc.js',
-    'boincsite/templates/assets/js/services/diskUsageSvc.js',
-    'boincsite/templates/assets/js/services/hostInfoSvc.js',
-    'boincsite/templates/assets/js/services/taskSvc.js',
-    'boincsite/templates/assets/js/services/noticesSvc.js',
-    'boincsite/templates/assets/js/services/globalPreferencesSvc.js',
-    'boincsite/templates/assets/js/services/jsonServices.js',
-    'boincsite/templates/assets/js/services/allProjectListSvc.js',
-    'boincsite/templates/assets/js/services/attachProjectSvc.js',
-    'boincsite/templates/assets/js/services/detachProjectSvc.js',
-    'boincsite/templates/assets/js/services/md5Svc.js',
-	 'boincsite/templates/assets/js/services/statisticsSvc.js',
-	 'boincsite/templates/assets/js/services/getPlatformSvc.js'	 
+    'boincsite/templates/assets/js/services/*.js',    
   ];
 
   var directives = [
@@ -60,19 +29,19 @@ module.exports = function(grunt) {
       controllers: {
         options: {
           sourceMap: true,
-          sourceMapName: 'boincsite/templates/assets/js/controllers/controller.js.map'
+          sourceMapName: 'boincsite/templates/assets/js/controller.js.map'
         },
         files: {
-          'boincsite/templates/assets/js/controllers/controller.min.js': controllers
+          'boincsite/templates/assets/js/controller.min.js': controllers
         }
       },
       services: {
         options: {
           sourceMap: true,
-          sourceMapName: 'boincsite/templates/assets/js/services/services.js.map'
+          sourceMapName: 'boincsite/templates/assets/js/services.js.map'
         },
         files: {
-          'boincsite/templates/assets/js/services/services.min.js': services
+          'boincsite/templates/assets/js/services.min.js': services
         }
       },
 		directives: {
