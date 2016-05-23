d2b37c61bb2ea0dc23ac2e0eae214ac5878ee945
jeroanan <david.tosslehoff@gmail.com>
2016-05-23 21:47:24 +0100

    Status 4 -> Uploading

--- boincsite/templates/assets/js/controllers/controller.js
+++ boincsite/templates/assets/js/controllers/controller.js
@@ -2,17 +2,17 @@ var upArrow = '&#x25B2;';
 var downArrow = '&#x25BC;';
 
 function getSortFunc(viewModel, sortFieldVar, reverseSortVar) {
-
+  
   var currentSortField = viewModel[sortFieldVar];
   
   function sortFunc(field) {
 	 viewModel[sortFieldVar] = field;
-
+	 
 	 if(field===currentSortField) {
 		viewModel[reverseSortVar] = !viewModel[reverseSortVar];
 		return;
 	 }
-
+	 
 	 currentSortField = field;
 	 viewModel[sortFieldVar] = field;
 	 viewModel[reverseSortVar] = false;
@@ -22,19 +22,19 @@ function getSortFunc(viewModel, sortFieldVar, reverseSortVar) {
 }
 
 var get_project_name = function(task, projects) {
-
+  
   for (var p in projects) {
     var proj = projects[p];
-
+	 
     if (proj.master_url == task.project_url){
-
+		
       if (proj.name==='') {
         return task.project_url;
       }
       return proj.name;
     }
   }
-
+  
   return task.project_url;
 };
 
@@ -44,22 +44,25 @@ var get_time_so_far = function(task) {
 
 var get_state_string = function(task) {
   if (task.suspended_via_gui) return 'Task suspended by user';
-
+  
   if (task.state === 2 && (task.scheduler_state === 1 || task.active_task_state === 0)) {
     return 'Waiting to run';
   }
-
+  
   switch (task.state) {
-    case 2:
-      return 'Running';
-    case 3:
-      return 'Computation error';
-    case 5:
-      return 'Ready to report';
-    case 6:
-      return 'Aborted by user';
-    default:
-      return task.state;
+  case 2:
+    return 'Running';
+  case 3:
+    return 'Computation error';
+  case 4:
+	 console.log(task);
+	 return 'Uploading';
+  case 5:
+    return 'Ready to report';
+  case 6:
+    return 'Aborted by user';
+  default:
+    return task.state;
   }
 };
 
@@ -68,17 +71,13 @@ function getQueryStrings() {
   var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
   var queryString = location.search.substring(1);
   var keyValues = queryString.split('&');
-
+  
   for(var i in keyValues) {
     var key = keyValues[i].split('=');
     if (key.length > 1) {
       assoc[decode(key[0])] = decode(key[1]);
     }
   }
-
+  
   return assoc;
 }
-
-
-
-
