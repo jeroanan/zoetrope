84bbe502a421d46a0544484ec2b9f8a692c78a12
jeroanan <david.tosslehoff@gmail.com>
2016-06-16 21:24:07 +0100

    Documentation

--- boincsite/boinc/UserTasks.py
+++ boincsite/boinc/UserTasks.py
@@ -1,6 +1,18 @@
 # Copyright (c) David Wilson 2016
-#
-# Licensed under the GPL version 3
+# This file is part of Zoetrope.
+# 
+# Zoetrope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Zoetrope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Zoetrope.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import sqlite3
@@ -20,7 +32,21 @@ class UserTasks(object):
     """
 
     def add_user(self, user_id, password):
+        """
+        Add a new user to Zoetrope.
 
+        The user_id cannot contain the pipe character and must be unique. If either of these conditions are not met then 
+        the new user will not be created.
+
+        Params:
+        user_id: An login name for the user. 
+        password: A password for the new user.
+
+        Returns:
+        An instance of SuccessError. If the user was saved successfully then its success flag will be set to True.
+        Otherwise the success flag will be False and the error_message property will describe why the user was not
+        added.
+        """
         ret_val = se.SuccessError()
 
         if user_id.find('|') > -1:
@@ -48,6 +74,12 @@ class UserTasks(object):
         return ret_val
 
     def get_users(self):
+        """
+        Get all users stored in Zoetrope
+        
+        Returns:
+        A list of User objects representing each user.
+        """
         conn, cursor = self.get_connection()
 
         sql = 'SELECT RowId, UserId FROM User'
@@ -63,16 +95,26 @@ class UserTasks(object):
         cursor.close()
         return users
 
-    def delete_user(self, user_id):
+    def delete_user(self, user_no):
+        """
+        Delete a user from Zoetrope
+
+        Params:
+        user_no: The user_no represents the RowId in Sqlite of the user's record.
+
+        Returns:
+        An instance of SuccessError. If all goes well this will be true and the error_message field will be set to the 
+        user_no.
+        """
         ret_val = se.SuccessError()
         
         conn, cursor = self.get_connection()
 
         sql = 'DELETE FROM User WHERE RowId=?'
-        cursor.execute(sql, (user_id,))
+        cursor.execute(sql, (user_no,))
 
         ret_val.success = True
-        ret_val.error_message = user_id
+        ret_val.error_message = user_no
 
         conn.commit()
         cursor.close()
@@ -80,6 +122,22 @@ class UserTasks(object):
         return ret_val
 
     def change_password(self, user_no, password, confirm_password):
+        """
+        Change the password of a user stored in Zoetrope.
+
+        A password and confirmation password must be supplied and be identical. If neither of these conditions are met
+        then the password will not be changed.
+
+        Params:
+        user_no: The RowId of the Sqlite record of the user to change the password for
+        password: The user's new password
+        confirm_password: The confirmation of the user's new password
+
+        Returns:
+        An instance of SuccessError. If the password was changed successfully then the success flag will be set to True
+        and the error_message field will be set to user_no. If the password was not changed successfully then the 
+        success flag will be set to False and the error_message field will describe what went wrong.
+        """
         ret_val = se.SuccessError()
 
         if password == '' or confirm_password == '':
