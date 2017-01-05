dfb5224379b336d0781dd47f5d19c7182aa7fd5a
David Wilson <davidwil@posteo.de>
2017-01-05 20:28:12 +0000

    Unit tests for detachProjectCtrl

--- boincsite/app/controllers/detachProjectCtrl.js
+++ boincsite/app/controllers/detachProjectCtrl.js
@@ -1,38 +1,53 @@
 /**
  * Controller for the detach project screen.
  *
- * (c) David Wilson 2016, licensed under GPL V3.
+ * Copyright (c) David Wilson 2016-2017
+ * This file is part of Zoetrope.
+ * 
+ * Zoetrope is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Zoetrope is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Zoetrope.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 angular.module('zoetropeControllers')
  .controller('detachProjectCtrl', DetachProjectController);
 
-DetachProjectController.$inject = ['projectSvc'];
+DetachProjectController.$inject = ['projectSvc', '$document', '$location'];
 
-function DetachProjectController(projectSvc) {
+function DetachProjectController(projectSvc, $document, $location) {
 
   var vm = this;
   vm.attachedProjects = null;
   vm.ready = false;
   vm.showConfirmDetach = false;
   vm.selectedProject = null;
-  vm.detachButtonClicked = confirmDetach;
-  vm.detachLinkClicked = detachLinkClicked;
   vm.title = 'Detach Project';
   vm.detachSuccessful = false;
   vm.detachClicked = false;
   vm.detachErrorMessage = '';
+
+  vm.detachButtonClicked = confirmDetach;
+  vm.detachLinkClicked = detachLinkClicked;
   vm.selectedProjectChanged = selectedProjectChanged;
 
-  projectSvc.getAttachedProjects(gotProjects);
+  projectSvc.getAttachedProjects(gotProjects); //TODO: No error handler here
   
-  document.title = vm.title;
+  $document[0].title = vm.title;
 
   function gotProjects(projects) {
 
     if (projects.length>0 && projects[0].error_message && projects[0].error_message===-1414)
     {
-      document.location = '/#/login';
+      $location.path('/#/login');
       return;
     }
 
@@ -59,13 +74,13 @@ function DetachProjectController(projectSvc) {
   // from the project.
   function detachLinkClicked() {
     vm.detachClicked = true;
-    projectSvc.detachProject(vm.selectedProject, projectDetached);
+    projectSvc.detachProject(vm.selectedProject, projectDetached); //TODO: No error handling
   }
 
   function projectDetached(d) {
 
     if (d.error_message===-1414) {
-      document.location = '/#/login';
+      $location.path('/#/login');
       return;
     }
 
